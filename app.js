06b77ee20ce24d0230f6a6f6757a98d340b294d0
Ivan <miniglez.98@gmail.com>
2024-09-05 21:01:36 +0200

    Conseguido en app.js

--- app.js
+++ app.js
@@ -1,13 +1,103 @@
 const express = require("express")
 const app = express()
+const axios = require("axios")
+const cheerio = require("cheerio")
+const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 const PORT = 3000
 
-const index = require("./index.js")
+const setData = (myData) => {
+    const template = 
+    `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+        <h1>Elementos Scrapeados</h1>
+        ${
+            myData.map(element => {
+                const template2 = 
+                `
+                    <h2>${element.h1}</h2>
+                    <h3>Urls de imagenes</h3>
+                    <ul>
+                        ${element.img.map(element2 => {
+                            return `<li>${element2}</li>`
+                            })}
+                    </ul>
+                    <h3>Textos</h3>
+                    <ul>
+                        ${element.p.map(element2 => {
+                            return `<li>${element2}</li>`
+                            }).join("")}
+                    </ul>
+                `
+                return template2
+            }).join("")
+        }   
+        </body>
+        </html>
+    `
+    console.log(template)
+    return template
+}
 
-index()
+app.get("/", async (req, res) => {
+    try{
+        const response = await axios.get(url)
+        if(response.status === 200){
+            const html = response.data
+            const $ = cheerio.load(html)
+            const links = []
+            $("#mw-pages a").each((index, element) => {
+                const link = $(element).attr("href")
+                links.push(link)
+            })
 
-//app.use("/", index)
+            const allData = links.map(async (ele) => {
+                try{
+                    const response2 = await axios.get("https://es.wikipedia.org"+ele)
+                    if(response2.status === 200){
+                        const html2 = response.data
+                        const $2 = cheerio.load(html2)
 
-// app.listen(PORT, () => {
-//     console.log(`http://localhost${PORT}`)
-// })
+                        const imgs = []
+                        const ps = []
+                        $2("img").each((index, element) => {
+                            const img = $2(element).attr("src")
+                            imgs.push(img)
+                        })
+
+                        $2("p").each((index, element) => {
+                            const p = $2(element).text()
+                            ps.push(p)
+                        })
+
+                        return {
+                            h1: $2("h1").text(),
+                            img: imgs,
+                            p: ps
+                        }
+                    }
+                }
+                catch(error){
+                    console.log(`Dio error el elemento ${ele}`)
+                }
+            })
+            const myData = await Promise.all(allData)
+            const template = setData(myData)
+            res.send(template)
+        }
+    }
+    catch(error){
+        console.log("La peticion principal fallo")
+    }
+
+})//Final Funcion
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
-const url = "https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap"
+const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 
 const cosa = async(app) => {
     axios.get(url).then(async (res) => {
@@ -14,16 +14,14 @@ const cosa = async(app) => {
                     links.push(link)
             }
             })
-            const allElements = await Promise.all(getAllData(links))
-            console.log(allElements)
-
+            
             const allData = links.map(async (ele) => {
                 const allData = await Promise.all(axios.get("https://es.wikipedia.org"+ele).then(response => {
                     const html2 = response.data
                     const $2 = cheerio.load(html2)
                     const imgs = []
                     const ps = []
-
+                    
                     $2("img").each((index, element) => {
                         const img = $(element).attr("src")
                         imgs.push(img)
@@ -33,7 +31,7 @@ const cosa = async(app) => {
                         const p = $(element).text()
                         ps.push(p)
                     })
-
+                    
                     const element = {
                         h1:  $2("h1").text(),
                         imgs: imgs,
@@ -43,11 +41,13 @@ const cosa = async(app) => {
                     
                 }).catch(() => "Falla"))
                 return allData
-        })
-
+            })
+            
+            const allElements = await Promise.all(getAllData(links))
+            console.log(allElements)
             
         }
-}).catch(() => "Falla")}
+    }).catch(() => "Falla")}
 
 const getAllData = (links) => {
     
